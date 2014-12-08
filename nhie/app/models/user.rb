0198d818e4a98fa163c75efb12ec26507a4bcc3f
Kristel Teng <kristelteng@gmail.com>
2014-12-08 14:14:06 -0500

    Install sorcery gem, validation in user model

--- nhie/app/models/user.rb
+++ nhie/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
+
   has_many :events
-  
 end
