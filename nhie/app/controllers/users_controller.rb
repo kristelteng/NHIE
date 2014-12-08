60c886036dbf4c9de884f2132044b57345294f26
Kristel Teng <kristelteng@gmail.com>
2014-12-08 14:20:29 -0500

    added in user_controller: new, create, show, user_params

--- nhie/app/controllers/users_controller.rb
+++ nhie/app/controllers/users_controller.rb
@@ -3,9 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to events_url, {notice: "Signed up!"}
+    else
+      render "new"
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def show
@@ -15,5 +23,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :username, :image)
   end
 end
