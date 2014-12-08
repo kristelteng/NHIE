4f5a9783f98499e8886298edb5702c5d547c565c
Kristel Teng <kristelteng@gmail.com>
2014-12-08 13:44:48 -0500

    added events - index, destroy, new

--- nhie/app/controllers/events_controller.rb
+++ nhie/app/controllers/events_controller.rb
@@ -1,8 +1,15 @@
 class EventsController < ApplicationController
   def index
+    @events = if params[:search]
+      Events.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+    else
+      Event.order("events.created_at DESC")
+    end.page(params[:page])
+    
   end
 
   def new
+    @event = Event.new
   end
 
   def create
@@ -12,8 +19,12 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to event_path
   end
 end
