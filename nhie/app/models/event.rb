8f025333f49ad569d6c4464ef523043334ad6a79
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-12-09 15:42:21 -0500

    added validation for event name,  description, event datetime, name. added event's info on event show page

--- nhie/app/models/event.rb
+++ nhie/app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ActiveRecord::Base
+  validates :event_name, presence: true
+  validates :description, presence: true
+  validates :event_datetime, presence: true
+  validates :event_name, presence: true
+
   belongs_to :user
   has_many :users
   
