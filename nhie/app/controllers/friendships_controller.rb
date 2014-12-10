039b13c20a442451e59d4af3fea47d21ac2f3838
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-12-10 10:06:45 -0500

    added logic into the friendship controller(not completed yet). Added link into layout that goes to profile page

--- nhie/app/controllers/friendships_controller.rb
+++ nhie/app/controllers/friendships_controller.rb
@@ -1,5 +1,11 @@
 class FriendshipsController < ApplicationController
   def new
+    if params[:friend_id]
+      @friend = User.find(params[:friend_id])
+      @friendship = current_user.friendships.new(friend: @friend)
+    else
+      flash[:error] = 'Friend Required'
+    end
   end
 
   def create
