82c7894d7b24a039aaa0acccaa5336b5aaf8bb63
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-12-11 14:10:06 -0500

    Changed links, added user id in events migration. showed a friends events. added awesome print and better errors.

--- nhie/db/schema.rb
+++ nhie/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210233050) do
+ActiveRecord::Schema.define(version: 20141211183557) do
 
   create_table "events", force: true do |t|
     t.text     "description"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141210233050) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "event_name"
+    t.integer  "user_id"
   end
 
   create_table "friendships", force: true do |t|
