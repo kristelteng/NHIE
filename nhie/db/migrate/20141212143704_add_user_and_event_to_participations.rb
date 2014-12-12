dcc7d50178fcb4c7327adce5c2b8048b2e2c676e
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-12-12 09:38:57 -0500

    added event and user id's to participations

---
+++ nhie/db/migrate/20141212143704_add_user_and_event_to_participations.rb
@@ -0,0 +1,6 @@
+class AddUserAndEventToParticipations < ActiveRecord::Migration
+  def change
+  	add_column :participations, :user_id, :integer
+  	add_column :participations, :event_id, :integer
+  end
+end
