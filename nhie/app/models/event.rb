ca3787b0868dcf0af438b4c751752cd63bb950ab
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-12-12 09:43:59 -0500

    removed old user associations from event model

--- nhie/app/models/event.rb
+++ nhie/app/models/event.rb
@@ -4,9 +4,6 @@ class Event < ActiveRecord::Base
   validates :event_datetime, presence: true
   validates :event_name, presence: true
 
-  belongs_to :user
-  has_many :users
-
   belongs_to :creator, class_name: 'User'
   has_many :participation
   has_many :participants, through: :participation, source: :user
