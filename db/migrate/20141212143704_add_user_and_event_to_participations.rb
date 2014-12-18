36979abc161a0e64c8798a6c364240525efeaf56
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-12-18 17:45:42 -0500

    Fixed a migration

--- db/migrate/20141212143704_add_user_and_event_to_participations.rb
+++ db/migrate/20141212143704_add_user_and_event_to_participations.rb
@@ -1,6 +1,10 @@
 class AddUserAndEventToParticipations < ActiveRecord::Migration
   def change
-  	add_column :participations, :user_id, :integer
-  	add_column :participations, :event_id, :integer
+	  create_table "participations", force: true do |t|
+	    t.datetime "created_at"
+	    t.datetime "updated_at"
+	    t.integer  "user_id"
+	    t.integer  "event_id"
+	  end
   end
 end
